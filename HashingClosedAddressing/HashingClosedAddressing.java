599fd6ed7bc7fb60f4251ea24c9ac56cbd4976d4
Prerak Shah <prerak9359@gmail.com>
2025-12-23 14:18:27 +1030

    Working on Closed Addressing.

---
+++ HashingClosedAddressing/HashingClosedAddressing.java
@@ -0,0 +1,157 @@
+// Hashing via Chaining...
+package HashingClosedAddressing;
+import java.util.ArrayList;
+
+class Node{
+    String key;
+    int value;
+    Node next;
+
+    Node(String key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LL{
+    Node first = null;
+    int n = 0;
+
+    void addTail(String key, int value){
+        Node new_node = new Node(key, value);
+        Node current = first;
+        if(first == null){
+            new_node.next = null;
+            first = new_node;
+            return;
+        }
+
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = new_node;
+    }
+
+    void remove(String key){
+        Node current = first;
+        if(first == null){
+            return;
+        }
+
+        if(key.equals(current.key)){
+            first = first.next;
+            return;
+        }
+
+        while(current.next != null){
+            if(key.equals(current.next.key)){
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+        return;
+    }
+
+    int search(String key){
+        Node current = first;
+        int pos = 0;
+
+        while(current != null){
+            if(key.equals(current.key)){
+                return pos;
+            }
+            pos += 1;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    Node get_node(int index){
+        Node current = first;
+        int pos = 0;
+
+        while(current != null){
+            if(pos == index){
+                return current;
+            }
+            pos += 1;
+            current = current.next;
+        }
+        return null;      
+    }
+
+    String traverse(){
+        Node current = first;
+        String information = "";
+        while(current != null){
+            information = information + current.key + " : " + current.value + ", ";
+            current = current.next;
+        }
+        return information.substring(0, information.length() - 2);
+    }
+}
+
+class Dictionary{
+    int capacity;
+    int size = 0;
+    ArrayList<LL> buckets;
+
+    Dictionary(int capacity){
+        this.capacity = capacity;
+        this.buckets = make_array(capacity);
+    }
+
+    ArrayList<LL> make_array(int capacity){
+        ArrayList<LL> parent = new ArrayList<>(capacity);;
+        for(int i = 0; i < capacity; i++){
+            parent.add(new LL());  
+        }
+        return parent; 
+    }
+
+    void put(String key, int value){
+
+        int bucket_index = hash_function(key);
+        int node_index = get_node_index(bucket_index, key);
+        if(node_index == -1){
+            // insert
+                buckets.get(bucket_index).addTail(key, value); 
+                size += 1;
+        }else{
+            // update
+            Node node = buckets.get(bucket_index).get_node(node_index);
+            node.value = value;
+        }
+        return;
+    }
+
+    int get_node_index(int bucket_index,String key){
+        int node_index = buckets.get(bucket_index).search(key);
+
+        return node_index;
+    }
+
+    int hash_function(String key){
+        return Math.abs(key.hashCode()) % capacity;
+    }
+}
+
+public class HashingClosedAddressing {
+    public static void main(){
+        LL l = new LL();
+        // l.addTail("Python", 20);
+        // l.addTail("Javascript", 30);
+        // l.addTail("Java", 40);
+        
+
+        Dictionary D1 = new Dictionary(4);
+
+        D1.put("Python", 34);
+        D1.put("Java", 33);
+        D1.put("JavaScript", 33);
+        D1.put("Php", 33);
+        D1.put("Java", 23);
+        System.out.println(D1.buckets.get(2).traverse());
+    }
+}
