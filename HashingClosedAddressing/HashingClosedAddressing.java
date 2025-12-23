c9c6ce199c1d3a0b7977428e4e91b5fc354cdbb9
Prerak Shah <prerak9359@gmail.com>
2025-12-23 16:48:48 +1030

    increased capacity successfully.

--- HashingClosedAddressing/HashingClosedAddressing.java
+++ HashingClosedAddressing/HashingClosedAddressing.java
@@ -118,6 +118,12 @@ class Dictionary{
             // insert
                 buckets.get(bucket_index).addTail(key, value); 
                 size += 1;
+
+                double loadFactor = (double) size / capacity;
+                System.out.println(loadFactor);
+                if(size / capacity >= 2){
+                    rehash();
+                }
         }else{
             // update
             Node node = buckets.get(bucket_index).get_node(node_index);
@@ -132,6 +138,26 @@ class Dictionary{
         return node_index;
     }
 
+    void rehash(){
+        capacity = capacity * 2;
+        ArrayList<LL> old_buckets = buckets;
+        size = 0;
+        buckets = make_array(capacity);
+
+        for(int i = 0; i < old_buckets.size(); i++){
+            int count = 0;
+            while(old_buckets.get(i).get_node(count) != null){
+                String key = old_buckets.get(i).get_node(count).key;
+                int value = old_buckets.get(i).get_node(count).value;
+                this.put(key, value);
+                count += 1 ;
+            }
+            // Node n = old_buckets.get(i).get_node(i);
+        }
+
+        }
+    
+
     int hash_function(String key){
         return Math.abs(key.hashCode()) % capacity;
     }
@@ -139,7 +165,7 @@ class Dictionary{
 
 public class HashingClosedAddressing {
     public static void main(){
-        LL l = new LL();
+        // LL l = new LL();
         // l.addTail("Python", 20);
         // l.addTail("Javascript", 30);
         // l.addTail("Java", 40);
@@ -151,7 +177,21 @@ public class HashingClosedAddressing {
         D1.put("Java", 33);
         D1.put("JavaScript", 33);
         D1.put("Php", 33);
-        D1.put("Java", 23);
-        System.out.println(D1.buckets.get(2).traverse());
+        D1.put("Javaa", 23);
+        D1.put("Ruby", 33);
+        D1.put("Rails", 33);
+        D1.put("C", 23);
+        D1.put("Pythonn", 34);
+        D1.put("Javaaa", 33);
+        D1.put("JavaScripta", 33);
+        D1.put("Phpa", 33);
+        D1.put("Javaaaa", 23);
+        D1.put("Rubya", 33);
+        D1.put("Railsa", 33);
+        D1.put("Javaaaaa", 23);
+        // D1.put("Rubyaa", 33);
+        // D1.put("Railsaa", 33);
+        
+        System.out.println(D1.buckets.size());
     }
 }
