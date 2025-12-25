be3402e7721e1e2fa485d58904ed5eb9dc004b5b
Prerak Shah <prerak9359@gmail.com>
2025-12-25 13:39:47 +1030

    Made Adaptive --> Bubble Sort.

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -5,12 +5,17 @@ class Bubble{
 
     int[] sort(int[] arr){
         for(int i = 0; i < arr.length - 1; i++){
+            boolean swapped = false;
             for(int j = 0; j < arr.length - 1 - i; j++){
                 int temp = arr[j];
                 if(temp > arr[j + 1]){
                     arr[j] = arr[j + 1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped){
+                break;
             }
         }
 
@@ -21,7 +26,7 @@ class Bubble{
 public class BubbleSort {
     public static void main(){
         Bubble b1 = new Bubble();
-        int[] unsortedArray = {53, 41, 33, 27, 11};
+        int[] unsortedArray = {1, 2, 3, 4, 5};
         b1.sort(unsortedArray);
         System.out.println(Arrays.toString(unsortedArray));
      }
