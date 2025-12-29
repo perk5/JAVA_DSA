5bf77079161834f24cc2b8f952d4c01263b38cdc
Prerak Shah <prerak9359@gmail.com>
2025-12-29 21:07:59 +1030

    Working on RMergeSort

--- MergeSort/MergeSort.java
+++ MergeSort/MergeSort.java
@@ -1,74 +1,70 @@
-    package MergeSort;
-    import java.util.Arrays;
-    class MergeSorted{ 
-        
-        void mergeSorted(int[] arr, int l, int mid, int r){
+package MergeSort;
+import java.util.Arrays;
+class MergeSorted{ 
+    void mergeSorted(int[] arr, int l, int mid, int r){
 
-            int n1 = mid - l + 1;
-            int n2 = r - mid;
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
 
 
-            int arr1[] = new int[n1];
-            int arr2[] = new int[n2];
+        int arr1[] = new int[n1];
+        int arr2[] = new int[n2];
 
-            for(int x = 0; x < n1; x++){
-                arr1[x] = arr[l+x];
-            }
-            for(int x = 0; x < n2; x++){
-                arr2[x] = arr[mid+1+x];
-            }
+        for(int x = 0; x < n1; x++){
+            arr1[x] = arr[l+x];
+        }
+        for(int x = 0; x < n2; x++){
+            arr2[x] = arr[mid+1+x];
+        }
 
-            int i = 0;
-            int j = 0;
-            int k = l;
+        int i = 0;
+        int j = 0;
+        int k = l;
 
-            while(i < n1 && j < n2){
-                if(arr1[i] < arr2[j]){
-                    arr[k] = arr1[i];
-                    i += 1;    
-                }else{
-                    arr[k] = arr2[j];
-                    j += 1;          
-                }
-                k++;
-            }
-            
-            while(i < n1){
+        while(i < n1 && j < n2){
+            if(arr1[i] < arr2[j]){
                 arr[k] = arr1[i];
-                i += 1;
-                k++;
-            }
-
-            while(j < n2){
+                i += 1;    
+            }else{
                 arr[k] = arr2[j];
-                j += 1;
-                k++;
+                j += 1;          
             }
-            System.out.println(Arrays.toString(arr));
+            k++;
+        }
+        
+        while(i < n1){
+            arr[k] = arr1[i];
+            i += 1;
+            k++;
         }
 
-        void mergeSort(int[] arr, int l , int r){
-    
-            if(l<r)
-            {
-                int mid = l + (r - l) / 2;
-
-                mergeSort(arr, l, mid);
-                mergeSort(arr, mid + 1, r);
-
-                mergeSorted(arr, l, mid, r );
-            }
+        while(j < n2){
+            arr[k] = arr2[j];
+            j += 1;
+            k++;
         }
+        System.out.println(Arrays.toString(arr));
     }
 
-    public class MergeSort {
+    void mergeSort(int[] arr, int l , int r){
+        if(l<r)
+        {
+            int mid = l + (r - l) / 2;
 
-        public static void main(String[] args){
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
 
-            int[] arr = {100, 4, 12, 10, 11, 8, 9, 7};
-            int l = 0;
-            int r = arr.length - 1;
-            MergeSorted m1 = new MergeSorted();
-            m1.mergeSort(arr, l, r);
+            mergeSorted(arr, l, mid, r );
         }
     }
+}
+
+public class MergeSort {
+    public static void main(String[] args){
+        int[] arr = {100, 4, 12, 10, 11, 8, 9, 7};
+        int l = 0;
+        int r = arr.length - 1;
+        MergeSorted m1 = new MergeSorted();
+        m1.mergeSort(arr, l, r);
+    }
+}
---
+++ RMergeSort/RMergeSort.java
@@ -0,0 +1,55 @@
+package RMergeSort;
+import java.util.Arrays;
+
+public class RMergeSort {
+
+    public static void mergeSort(int[] data, int start, int end){
+        if (start < end){
+            int mid = start + (end - start) / 2;
+            mergeSort(data, start, mid);
+            mergeSort(data, mid + 1, end);
+            merge(data, start, mid, end);  
+        }
+    }
+
+    public static int[] merge(int[] data, int start, int mid, int end){
+        // Building temp array to avoid modifying the original content....
+
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+
+        // While both sub-arrays have values, then try and merge them in sorted order....
+
+        while(i <= mid && j <= end){
+            if(data[i] <= data[j]){
+                temp[k] = data[i];
+                i++;
+                k++;
+            }else{
+                temp[k] = data[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i <= mid){
+            temp[k++] = data[i++];
+        }
+         while(j <= end){
+            temp[k++] = data[j++];
+        }
+
+        for(i = start; i<= end; i++){
+            data[i] = temp[i - start];
+        }
+        System.out.println(Arrays.toString(data));
+        return data;
+    }
+
+
+    public static void main(String args[]){
+        int[] arr = new int[] {4, 6, 2, 1, 8, 9};
+
+        mergeSort(arr, 0, arr.length - 1);
+    }
+}
