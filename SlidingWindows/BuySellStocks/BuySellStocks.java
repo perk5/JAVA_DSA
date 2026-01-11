591b202fd09fa416b91a73ac15b958acecb6aaa8
Prerak Shah <prerak9359@gmail.com>
2026-01-12 09:38:21 +1030

    Completed BuyingandSelling Stocks in T.C O(n) S.C O(1)

--- SlidingWindows/BuySellStocks/BuySellStocks.java
+++ SlidingWindows/BuySellStocks/BuySellStocks.java
@@ -2,10 +2,22 @@ package SlidingWindows.BuySellStocks;
 
 public class BuySellStocks {
 
-    public static int add(String a,String b){
-        return 0;
+    public static int maxProfit(int[] stocksP){
+        int max = stocksP[0];
+        int profit = 0;
+
+        for(int i = 0; i < stocksP.length; i++){
+            if(stocksP[i] < max){
+                max = stocksP[i];
+            }
+            profit = Math.max(profit, stocksP[i] - max);
+        }
+        return profit;
     }
 
     public static void main(String args[]){
+
+        int[] stocksPrice = new int[]{7,1,3,6,2};
+        System.out.println(maxProfit(stocksPrice));
     }
 }
