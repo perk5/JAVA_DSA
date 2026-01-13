09b8c6636ba5c37062063c4ddf0ce2611e058817
Prerak Shah <prerak9359@gmail.com>
2026-01-13 14:24:47 +1030

    Done with Verifying Permutations. T.C O(n) S.C O(1).

--- SlidingWindows/LongestCReplacement/LongestCReplacement.java
+++ SlidingWindows/LongestCReplacement/LongestCReplacement.java
@@ -3,30 +3,32 @@ package SlidingWindows.LongestCReplacement;
 public class LongestCReplacement {
 
     public static int LongestCharactersReplacement(String word, int k){
-        int[] characters = new int[26];
-        char[] arr = word.toCharArray();
+        // int[] characters = new int[26];
+        // char[] arr = word.toCharArray();
 
-        if(k >= word.length()){
-            return 0;
-        }
+        // if(k >= word.length()){
+        //     return 0;
+        // }
 
-        for(int i = 0; i < word.length(); i++){
-            characters[word.charAt(i) - 'a']++;
-        }
+        // for(int i = 0; i < word.length(); i++){
+        //     characters[word.charAt(i) - 'a']++;
+        // }
 
-        for(int i = 0; i < 26; i++){
-            if(characters[i] > 1){
-               return 0;
-            }
-        }
+        // for(int i = 0; i < 26; i++){
+        //     if(characters[i] > 1){
+        //        return 0;
+        //     }
+        // }
 
-        for(int i = 1; i < k + 1; i++){
-            arr[i] = arr[0];
-        }
+        // for(int i = 1; i < k + 1; i++){
+        //     arr[i] = arr[0];
+        // }
        
-        word = new String(arr);
-        System.out.println(word);
-        return k+1; 
+        // word = new String(arr);
+        // System.out.println(word);
+        // return k+1; 
+
+        
     }
 
     public static void main(String args[]){
--- SlidingWindows/VerifyingPermutations/VerifyingPermutations.java
+++ SlidingWindows/VerifyingPermutations/VerifyingPermutations.java
@@ -1,7 +1,44 @@
 package SlidingWindows.VerifyingPermutations;
 
 public class VerifyingPermutations {
-    public static void main(String args[]){
 
+    public static boolean permutation(String s1, String s2){
+
+        if(s1.length() > s2.length()){
+            return false;
+        }
+
+        int[] mapS1 = new int[26];
+        int[] mapS2 = new int[26];
+
+        for(int i = 0; i < s1.length(); i++){
+            mapS1[s1.charAt(i) - 'a']++;
+            mapS2[s2.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < s2.length() - s1.length(); i++){
+            if(matches(mapS1, mapS2)){
+                return true;
+            }
+            mapS2[s2.charAt(i+s1.length()) - 'a']++;
+            mapS2[s2.charAt(i) - 'a']--;
+        }
+
+        return matches(mapS1, mapS2);
+    }
+
+    private static boolean matches(int[] map1, int[] map2){
+        for(int i = 0; i < 26; i++){
+            if(map1[i] != map2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String s1 = "ab";
+        String s2 = "cdncbrab";
+        System.out.println(permutation(s1, s2));
     }
 }
