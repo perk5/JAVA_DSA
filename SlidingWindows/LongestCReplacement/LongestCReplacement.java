45a039decd938a769a705ff7c20431f5088f46fa
Prerak Shah <prerak9359@gmail.com>
2026-01-14 12:34:30 +1030

    Solved Longest Character Replacement.

--- SlidingWindows/LongestCReplacement/LongestCReplacement.java
+++ SlidingWindows/LongestCReplacement/LongestCReplacement.java
@@ -3,8 +3,35 @@ package SlidingWindows.LongestCReplacement;
 public class LongestCReplacement {
 
     public static int LongestCharactersReplacement(String word, int k){
-        // int[] characters = new int[26];
-        // char[] arr = word.toCharArray();
+        int left = 0;
+        int right = 0;
+        int maxOccurance = 0;
+        int ans = 0;
+
+        int[] occurance = new int[26];
+
+        for(right = 0; right < word.length(); right++){
+            maxOccurance = Math.max(maxOccurance, ++occurance[word.charAt(right) - 'a']);
+
+            if(right - left + 1 - maxOccurance > k){
+                occurance[word.charAt(left) - 'a']--;
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }   
+        return ans;
+    
+    }
+
+    public static void main(String args[]){
+        String word = "abbac";
+        int k = 2;
+        System.out.println(LongestCharactersReplacement(word, k));
+    }
+}
+
+
+// char[] arr = word.toCharArray();
 
         // if(k >= word.length()){
         //     return 0;
@@ -27,13 +54,3 @@ public class LongestCReplacement {
         // word = new String(arr);
         // System.out.println(word);
         // return k+1; 
-
-        
-    }
-
-    public static void main(String args[]){
-        String word = "abcd";
-        int k = 4;
-        System.out.println(LongestCharactersReplacement(word, k));
-    }
-}
