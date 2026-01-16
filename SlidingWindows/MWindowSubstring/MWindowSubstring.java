2d0069d427da0995d435afb697db2e9557e93f4c
Prerak Shah <prerak9359@gmail.com>
2026-01-17 03:30:36 +1030

    Solved Minimum Window Substring problem T.C O(n)

---
+++ SlidingWindows/MWindowSubstring/MWindowSubstring.java
@@ -0,0 +1,60 @@
+package SlidingWindows.MWindowSubstring;
+
+import java.util.*;
+
+public class MWindowSubstring {
+
+    public static int minimumLength(String s, String t) {
+        int length = s.length();
+        int left = 0;
+        char[] tc = t.toCharArray();
+        char[] sc = s.toCharArray();
+
+        Map<Character, Integer> tHash = new HashMap<>();
+        Map<Character, Integer> subStringHash = new HashMap<>();
+
+        int unique = 0;
+        int create = 0;
+        for (char c : tc) {
+            if (!tHash.containsKey(c)) {
+                unique++;
+            }
+            tHash.put(c, tHash.getOrDefault(c, 0) + 1);
+        }
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char rChar = sc[right];
+            subStringHash.put(rChar, subStringHash.getOrDefault(rChar, 0) + 1);
+
+            if (tHash.containsKey(rChar) &&
+                    subStringHash.get(rChar).intValue() == tHash.get(rChar).intValue()) {
+                create++;
+            }
+            while (create == unique) {
+                length = Math.min(length, right - left + 1);
+                char lChar = sc[left];
+                subStringHash.put(lChar, subStringHash.get(lChar) - 1);
+
+                if (tHash.containsKey(lChar) &&
+                        subStringHash.get(lChar) < tHash.get(lChar)) {
+                    create--;
+                }
+
+                if (subStringHash.get(lChar) == 0) {
+                    subStringHash.remove(lChar);
+                }
+                left++;
+            }
+
+        }
+
+        return length;
+    }
+
+    public static void main(String args[]) {
+        String s = "avaabcabdc"; // sc
+        String t = "abc"; // tc
+        System.out.println(minimumLength(s, t));
+    }
+}
